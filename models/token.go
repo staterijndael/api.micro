db37bdb0b8932b67c2afd089a319d7db5ba48649
deissh <kudryavtsev@nppsatek.ru>
2019-05-02 09:06:36 +0300

    убрал сваггер их роута сервиса + добавил скоупы у токена + правки

--- models/token.go
+++ models/token.go
@@ -6,7 +6,23 @@ import (
 
 type Token struct {
 	gorm.Model
-	AccessToken  string `gorm:"unique;not null"`
-	RefreshToken string `gorm:"unique;not null"`
-	UserId       int    `gorm:"not null"`
+	AccessToken  string           `gorm:"unique;not null"`
+	RefreshToken string           `gorm:"unique;not null"`
+	UserId       int              `gorm:"not null"`
+	Permissions  TokenPermissions `gorm:"not null"`
+}
+
+type TokenPermissions struct {
+	Notify        bool `gorm:"default:false"`
+	Friends       bool `gorm:"default:false"`
+	Status        bool `gorm:"default:false"`
+	Messages      bool `gorm:"default:false"`
+	Comments      bool `gorm:"default:false"`
+	Wall          bool `gorm:"default:false"`
+	Notifications bool `gorm:"default:false"`
+	Email         bool `gorm:"default:false"`
+}
+
+func (t Token) AddScopes(scope []string) {
+
 }
--- service-auth/common/database.go
+++ service-auth/common/database.go
@@ -2,8 +2,9 @@ package common
 
 import (
 	"github.com/deissh/api.micro/models"
+	"github.com/deissh/api.micro/service-auth/helpers"
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/labstack/gommon/log"
 )
 
@@ -15,13 +16,20 @@ var DB *gorm.DB
 
 // Opening a database and save the reference to `Database` struct.
 func Init() *gorm.DB {
-	db, err := gorm.Open("sqlite3", "tests.db")
+	host := helpers.GetEnv("DB_HOST", "127.0.0.1")
+	user := helpers.GetEnv("DB_USER", "postgres")
+	dbName := helpers.GetEnv("DB_NAME", "microapi")
+	psw := helpers.GetEnv("DB_PSW", "postgres")
+
+	db, err := gorm.Open("postgres", "sslmode=disable host="+host+" user="+user+" dbname="+dbName+" password="+psw)
 	if err != nil {
 		log.Panic(err)
 	}
 	db.DB().SetMaxIdleConns(10)
 	//db.LogMode(true)
 	DB = db
+
+	log.Info("Database connected")
 	return DB
 }
 
